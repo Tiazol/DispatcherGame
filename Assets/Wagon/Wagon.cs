2d46a5317bc34eac60ecd9fbbc917ba2c64d85da
Vasiliy Makarov <v.makarov1357@gmail.com>
2020-07-31 09:18:59 +0300

    Создан прототип вагона

---
+++ Assets/Wagon/Wagon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wagon : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var direction = new Vector3(0, 1);
+        transform.Translate(direction * speed);
+    }
+}
