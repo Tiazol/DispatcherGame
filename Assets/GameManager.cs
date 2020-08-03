e2b0b7d223dc4efa0777f6e82a4562bd0a2894e3
Vasiliy Makarov <v.makarov1357@gmail.com>
2020-08-03 16:25:53 +0300

    Добавлено главное меню, плюс меню выбора уровня (пока загружается только первый уровень)

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private const string pp_unlockedLevels = "unlockedLevels";
+
     public static GameManager Instance { get; private set; }
     public Dictionary<GameObject, Checkpoint> Checkpoints { get; private set; }
+
+    public int UnlockedLevels { get; private set; }
+    public int TotalLevels { get; } = 15;
 
     public event Action ScoreChanged;
     private int score;
@@ -29,14 +35,29 @@ public class GameManager : MonoBehaviour
         Checkpoints = new Dictionary<GameObject, Checkpoint>();
     }
 
+    private void Start()
+    {
+        UnlockedLevels = PlayerPrefs.HasKey(pp_unlockedLevels) ? PlayerPrefs.GetInt(pp_unlockedLevels) : 1;
+    }
+
     private void Update()
     {
         if (Application.platform == RuntimePlatform.Android)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
-                Application.Quit();
+                Quit();
             }
         }
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
---
+++ Assets/MainMenuUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject levelSelection;
+    public GameObject levelButtons;
+    public Button levelButtonPrefab;
+
+    private void Start()
+    {
+        for (int i = 0; i < GameManager.Instance.TotalLevels; i++)
+        {
+            var j = i; // замыкание
+            var button = Instantiate(levelButtonPrefab, levelButtons.transform);
+            var text = button.GetComponentInChildren<Text>();
+            text.text = (j + 1).ToString();
+            button.onClick.AddListener(() => GameManager.Instance.LoadScene(j + 1));
+        }
+    }
+
+    public void ShowLevelSelection()
+    {
+        mainMenu.SetActive(false);
+        levelSelection.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        levelSelection.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
